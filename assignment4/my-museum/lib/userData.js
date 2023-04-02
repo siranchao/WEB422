78d4ec428b6b60237895af1c94c7d9f3b985d0e9
Siran Cao <siranchao@gmail.com>
2023-04-01 23:32:24 -0400

    add front-end auth

---
+++ assignment4/my-museum/lib/userData.js
@@ -0,0 +1,131 @@
+import { getToken } from './auth'
+
+
+export async function addToFavorites(itemID) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/favorites?id=${itemID}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+            "Authorization": `jwt ${getToken()}`
+        }
+    })
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+        console.log(`Add to favorites successful`);
+        return data
+    }
+    else {
+        console.log(data.error);
+        return [];
+    }
+}
+
+
+export async function removeFromFavorites(itemID) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/favorites?id=${itemID}`, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `jwt ${getToken()}`
+        }
+    })
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+        console.log(`removed from favorites`);
+        return data;
+    }
+    else {
+        console.log(data.error);
+        return [];
+    }
+
+
+}
+
+export async function getFavorites() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/favorites`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `jwt ${getToken()}`
+        }
+    })
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+        console.log(`fetch all favorites successful`);
+        return data;
+    }
+    else {
+        console.log(data.error);
+        return [];
+    }
+}
+
+export async function addToHistory(itemID) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/history?id=${itemID}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `jwt ${getToken()}`
+        }
+    })
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+        console.log(`add to history successful`);
+        return data;
+    }
+    else {
+        console.log(data.error);
+        return []
+    }
+}
+
+export async function removeFromHistory(itemID) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/history?id=${itemID}`, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `jwt ${getToken()}`
+        }
+    })
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+        console.log(`item removed from history`);
+        return data;
+    }
+    else {
+        console.log(data.error);
+        return [];
+    }
+}
+
+export async function getHistory() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/history`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `jwt ${getToken()}`
+        }
+    })
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+        console.log(`fetch all history`);
+        return data;
+    }
+    else {
+        console.log(data.error);
+        return [];
+    }
+}
