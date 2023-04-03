cd62a80649c5c9dac08631f81703aa23ce93bc8f
Siran Cao <siranchao@gmail.com>
2023-04-02 23:39:44 -0400

    submission

--- assignment4/my-museum/pages/index.js
+++ assignment4/my-museum/pages/index.js
@@ -1,13 +1,15 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
+*  WEB422 – Assignment 06
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
 * 
-*  Name: Siran Cao     Student ID: 159235209        Date: 03/18/2023
+*  Name: Siran Cao    Student ID: 159235209    Date: 03/30/2023
 *
+*  Vercel App (Deployed) Link: https://my-museum-siranchao.vercel.app/
 *
 ********************************************************************************/
+
 
 import Head from 'next/head'
 import { Row, Col, Image } from 'react-bootstrap'
